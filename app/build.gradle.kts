577cad66ab7338d548e8ee24a59cf848850ab21a
saeedhoseynzade42-dotcom <saeedhoseynzade42@gmail.com>
2026-08-15 03:33:51 +0330

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android { namespace="com.example.kaptanfree"; compileSdk=36
+ defaultConfig { applicationId="com.kavosh.scanner"; minSdk=26; targetSdk=36; versionCode=2; versionName="2.0" }
+}
